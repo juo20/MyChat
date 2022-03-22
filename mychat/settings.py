f959a1bd5573c9c91ece86de8b060036e01a9a87
juo20 <ugalde.jan@web.de>
2022-03-22 17:18:28 +0100

    adding postgresql

--- mychat/settings.py
+++ mychat/settings.py
@@ -89,15 +89,24 @@ WSGI_APPLICATION = 'mychat.wsgi.application'
 import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
-        }
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'DB_TEST',
+
+        'USER': 'DB_USER',
+
+        'PASSWORD': 'DB_PASSWORD',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
